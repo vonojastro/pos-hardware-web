ddb828e4894c2edd0930c7df72e6de2b8c3b3afa
Von Ojastro <von@Vons-MacBook-Pro.local>
2022-09-25 17:05:33 +0800

    first commit

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,14 @@ import connectDB from "./config/db.js"
 import { errorHandler, notFound } from "./middleware/middleware.js";
 import transactionRoutes from "./routes/transactionsRoutes.js"
 import path from 'path'
+import cors from "cors"
+
 
 dotenv.config()
 connectDB()
 const app = express()
 app.use(express.json());
+app.use(cors())
 
 app.use("/api/transactions", transactionRoutes);
 
