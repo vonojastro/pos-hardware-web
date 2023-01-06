590c2df300a9d9ec980a6eb3050204457e1a9f42
vonojastro <97124440+vonojastro@users.noreply.github.com>
2023-01-06 23:56:02 +0800

    development

--- client/src/page/Dashboard/Admin/index.jsx
+++ client/src/page/Dashboard/Admin/index.jsx
@@ -4,11 +4,7 @@ import React from 'react'
 const AdminDashboard = () => {
   return (
 <div>
-  <div className='h-[500px] overflow-hidden flex justify-center items-center'>
-
-  <img className='' src={require('../../../img/fam.jpg')} />
-  </div>
- 
+DashBoard
 </div>
   )
 }
